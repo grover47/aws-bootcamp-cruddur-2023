5a9ce8bc24c786355f8cfb2c6baad6b553d80e90
Charlie Grover <grover47@hotmail.com>
2023-02-23 15:34:01 +0000

    Update to class

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-class NotifitcationsActivities:
+class NotificationsActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
