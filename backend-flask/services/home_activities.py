1c19899e1b03c24284ae14ade075d3328f7a6ffc
Charlie Grover <grover47@hotmail.com>
2023-02-27 15:22:13 +0000

    Add spans and attributes

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,14 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
+    with tracer.start_as_current_span("home-activities-mock-data"):
+     span = trace.get_current_span()
+     now = datetime.now(timezone.utc).astimezone()
+     span.set_attribute("app.now", now.isoformat())
+     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
       'message': 'Cloud is very fun!',
@@ -41,4 +47,5 @@ class HomeActivities:
       'replies': []
     }
     ]
+    span.set_attribute("app.result_length", len(results))
     return results
