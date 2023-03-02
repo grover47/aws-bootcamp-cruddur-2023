dc165d73a0dedd7069ce91501df7f6324fb1cd34
Charlie Grover <grover47@hotmail.com>
2023-03-02 21:17:05 +0000

    Implement Cloudwatch Logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+    # Logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
      span = trace.get_current_span()
      now = datetime.now(timezone.utc).astimezone()
