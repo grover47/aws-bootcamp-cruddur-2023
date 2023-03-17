b8091f05172f26da09e8edf9e989b332b517611f
Charlie Grover <grover47@hotmail.com>
2023-03-17 10:26:53 +0000

    update seed and schema

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (email, display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  -- we can grab the cognito_user_id manually from the Congito Console
-  ('andrew@exampro.co', 'Andrew Brown', 'andrewbrown' ,'7659c102-7ad4-44bd-bdc3-208f934d1f41'),
-  ('bayko@exampro.co', 'Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'user@example.com', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'users@example.com', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
@@ -12,4 +11,3 @@ VALUES
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
-
