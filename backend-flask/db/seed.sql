81815f6fa660776e131b3cc4f29dcd782eda41fe
Charlie Grover <grover47@hotmail.com>
2023-03-28 16:20:05 +0000

    update seed.sql and ddb seed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,6 +1,7 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
+  ('Charlie Grover', 'charliegrover23@gmail.com', 'charliegrover', '9981b4c7-53b4-4308-b9af-a7dc0ac982e0')
   ('Andrew Brown', 'user@example.com', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'users@example.com', 'turtle47' ,'MOCK');
 
