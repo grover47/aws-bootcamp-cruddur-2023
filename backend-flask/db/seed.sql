71ff7cc4f80e3a3cfdd1db23414b5cf1bcb9141d
Charlie Grover <grover47@hotmail.com>
2023-03-28 22:23:32 +0000

    update MOCK

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,8 +2,8 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Frank Turtle','grover47@hotmail.com' , 'turtle47' ,'a64d86d4-8e70-45c5-96f7-5412e9c8ae9a'), 
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'a64d86d4-8e70-45c5-96f7-5412e9c8ae9a'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'a64d86d4-8e70-45c5-96f7-5412e9c8ae9a');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
