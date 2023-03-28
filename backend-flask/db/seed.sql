d265c3a509734fd7e7a15b98142789455a2319cb
Charlie Grover <grover47@hotmail.com>
2023-03-28 16:48:14 +0000

    update seed.sql

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Charlie Grover', 'charliegrover23@gmail.com', 'charliegrover', '9981b4c7-53b4-4308-b9af-a7dc0ac982e0')
-  ('Londo Mollari', 'lmollari@centari.com', 'londo' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
+  ('Charlie Grover','charliegrover23@gmail.com' , 'charliegrover' ,'9981b4c7-53b4-4308-b9af-a7dc0ac982e0'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
