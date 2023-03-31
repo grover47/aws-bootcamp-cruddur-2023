e530e34d44d5a6400898e32a88961653482f651a
Charlie Grover <grover47@hotmail.com>
2023-03-31 18:31:36 +0000

    update message_group

--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -1,51 +1,30 @@
-import './MessageGroupsPage.css';
-import React from "react";
+from datetime import datetime, timedelta, timezone
+from lib.ddb import Ddb
+from lib.db import db
+# from lib.momento import MomentoCounter
 
-import DesktopNavigation  from '../components/DesktopNavigation';
-import MessageGroupFeed from '../components/MessageGroupFeed';
-import checkAuth from '../lib/CheckAuth';
-
-export default function MessageGroupsPage() {
-  const [messageGroups, setMessageGroups] = React.useState([]);
-  const [popped, setPopped] = React.useState([]);
-  const [user, setUser] = React.useState(null);
-  const dataFetchedRef = React.useRef(false);
-  const loadData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
-      const res = await fetch(backend_url, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("access_token")}`
-        },
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setMessageGroups(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
+class MessageGroups:
+  def run(cognito_user_id):
+    model = {
+      'errors': None,
+      'data': None
     }
-  };  
+    
+    print("cognito_user_id in message_groups.py")
+    print(cognito_user_id)
+    
 
-React.useEffect(()=>{
-    //prevents double call
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
+    sql = db.template('users','uuid_from_cognito_user_id')
+    my_user_uuid = db.query_value(sql,{'cognito_user_id': cognito_user_id})
 
-    loadData();
-    checkAuth(setUser);
-  }, [])
-  return (
-    <article>
-      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
-      <section className='message_groups'>
-        <MessageGroupFeed message_groups={messageGroups} />
-      </section>
-      <div className='content'>
-      </div>
-    </article>
-  );
-}
+    print(f"UUID: {my_user_uuid}")
+    
+    ddb = Ddb.client()
+    data = Ddb.list_message_groups(ddb, my_user_uuid)
+    print("list_message_groups")
+    print(data)
+
+    #MomentoCounter.reset(f"msgs/{user_handle}")
+    model['data'] = data
+    return model
+ 
