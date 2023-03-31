cea11c03dafeceb90b4c00b5a698d0c902107a52
Charlie Grover <grover47@hotmail.com>
2023-03-31 18:29:22 +0000

    update users sql

--- backend-flask/db/sql/users/uuid_from_cognito_user_id.sql
+++ backend-flask/db/sql/users/uuid_from_cognito_user_id.sql
@@ -1,6 +1,6 @@
 SELECT
   users.uuid
 FROM public.users
-WHERE 
-  users.cognito_user_id = %(cognito_user_id)s
+WHERE
+  users.cognito_user_id= %(cognito_user_id)s
 LIMIT 1
--- backend-flask/services/message_groups.py
+++ backend-flask/services/message_groups.py
@@ -1,25 +1,51 @@
-from datetime import datetime, timedelta, timezone
+import './MessageGroupsPage.css';
+import React from "react";
 
-from lib.ddb import Ddb
-from lib.db import db
+import DesktopNavigation  from '../components/DesktopNavigation';
+import MessageGroupFeed from '../components/MessageGroupFeed';
+import checkAuth from '../lib/CheckAuth';
 
-class MessageGroups:
-  def run(cognito_user_id):
-    model = {
-      'errors': None,
-      'data': None
+export default function MessageGroupsPage() {
+  const [messageGroups, setMessageGroups] = React.useState([]);
+  const [popped, setPopped] = React.useState([]);
+  const [user, setUser] = React.useState(null);
+  const dataFetchedRef = React.useRef(false);
+  const loadData = async () => {
+    try {
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/message_groups`
+      const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        },
+        method: "GET"
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessageGroups(resJson)
+      } else {
+        console.log(res)
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };  
 
-    sql = db.template('users','uuid_from_cognito_user_id')
-    my_user_uuid = db.query_value(sql,{
-      'cognito_user_id': cognito_user_id
-    })
+React.useEffect(()=>{
+    //prevents double call
+    if (dataFetchedRef.current) return;
+    dataFetchedRef.current = true;
 
-    print(f"UUID: {my_user_uuid}")
-
-    ddb = Ddb.client()
-    data = Ddb.list_message_groups(ddb, my_user_uuid)
-    print("list_message_groups:",data)
-
-    model['data'] = data
-    return model
+    loadData();
+    checkAuth(setUser);
+  }, [])
+  return (
+    <article>
+      <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
+      <section className='message_groups'>
+        <MessageGroupFeed message_groups={messageGroups} />
+      </section>
+      <div className='content'>
+      </div>
+    </article>
+  );
+}
