f63c9be8882ed8cba3613e2231b13d311d567cca
Charlie Grover <grover47@hotmail.com>
2023-05-05 02:37:08 +0000

    update app.py

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -102,7 +102,8 @@ cors = CORS(
 
 # Rollbar ----------
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
+
+#@app.before_first_request
 def init_rollbar():
     """init rollbar module"""
     rollbar.init(
